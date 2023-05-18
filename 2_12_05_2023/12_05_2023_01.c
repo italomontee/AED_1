622007b97977743f3a37384f5c8be24c56b6d037
italomontee <120536529+italomontee@users.noreply.github.com>
2023-05-18 10:57:42 -0400

    Update 12_05_2023_01.c

--- 2_12_05_2023/12_05_2023_01.c
+++ 2_12_05_2023/12_05_2023_01.c
@@ -1,27 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
-
-typedef struct Cubo
-{
-    int aresta1;
-    int aresta2;
-    int aresta3;
-}Cubo;
-
-void PreencherDadosCubo(Cubo *cubo){
-    printf("ARESTA 1: ");
-    scanf("%d", &(cubo->aresta1));
-    printf("ARESTA 2: ");
-    scanf("%d", &(cubo->aresta2));
-    printf("ARESTA 3: ");
-    scanf("%d", &(cubo->aresta3));
-}
-
-void ValoresCubo(Cubo *cubo){
-    printf("Volume: %d\n", ((cubo->aresta1)*(cubo->aresta2)*(cubo->aresta3)));
-    printf("Area Total: %d\n", (2*((cubo->aresta1)*(cubo->aresta2))+2*((cubo->aresta1)*(cubo->aresta3))+2*((cubo->aresta2)*(cubo->aresta3))));
-
-}   
+#include "12_05_2023_01.h"
 
 int main(){
     Cubo *cubo = (Cubo *) malloc(sizeof(cubo));
