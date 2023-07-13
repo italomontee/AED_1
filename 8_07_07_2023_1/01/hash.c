aaddaaf8df23a5e8fd5135bcdee879f9594e41dc
italomontee <120536529+italomontee@users.noreply.github.com>
2023-07-13 03:25:41 -0400

    Update hash.c

--- 8_07_07_2023_1/01/hash.c
+++ 8_07_07_2023_1/01/hash.c
@@ -1,32 +1,56 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "hash.h"
+#define MAX 10
 
-int m_hasg (int cod_pess) {
+int m_hash (int cod_pess) {
     return (cod_pess % MAX);
 }
 
-void inicializarHash(TabelaHash tab){
+void inicializarHash(TabelaHash* tab){
 
     for (int i = 0; i < MAX; i++) {
-        tab.tabela[i] = NULL;
+        tab->tabela[i] = NULL;
     }
 }
 
-int inserir (TabelaHash tab, PESSOA pess) {
-
+int inserir(TabelaHash* tab, PESSOA pess) {
+    int h = m_hash(pess.codigo);
+    // ENCONTRAR CASA VAZIA
+    while (tab->tabela[h] != NULL) {
+        h = (h + 1) % MAX;
+    }
+    // ENCONTROU UMA CASA VAZIA
+    if (tab->tabela[h] == NULL) {
+        tab->tabela[h] = malloc(sizeof(PESSOA));
+        *(tab->tabela[h]) = pess;
+    }
+    return 0;
 }
 
-void mostrarHash (TabelaHash tab){
+void mostrarHash (TabelaHash* tab){
     for (int i = 0; i < MAX; i++) {
-        printf("nome: %s", tab.tabela[i]->nome);
+        if (tab->tabela[i] != NULL){
+            printf("nome: %s\n", tab->tabela[i]->nome);
+            printf("idade: %d\n", tab->tabela[i]->idade);
+            printf("codigo: %d\n", tab->tabela[i]->codigo);
+            printf("cpf: %d\n", tab->tabela[i]->cpf);
+            printf("\n");
+        } else {
+            printf("Passoa: %d NULL\n", i);
+            printf("\n");
+
+        }
+
     }
 }
 PESSOA * busca(TabelaHash tab, int cod){
-    for (int i = 0; i < MAX; i++) {
-        if(tab.tabela[i]->codigo == cod) {
-            printf("Encontrada a pessoa de codigo: %d", tab.tabela[i]->codigo);
-            return (tab.tabela[i]);
+    int h = m_hash(cod);
+    while (tab.tabela[h] != NULL){
+        if(tab.tabela[h]->codigo == cod){
+            return tab.tabela[h];
+        h = (h+1)% MAX;
         }
+        return NULL;
     }
 }
