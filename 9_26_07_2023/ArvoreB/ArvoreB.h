c5ed005d115c2e7862cce42bb4039884db5ff110
italomontee <igm.eng22@uea.edu.br>
2023-08-06 16:07:00 -0400

    BASE

--- 9_26_07_2023/ArvoreB/ArvoreB.h
+++ 9_26_07_2023/ArvoreB/ArvoreB.h
@@ -2,5 +2,19 @@
 #define ARVOREB_H_INCLUDED
 
 
+typedef struct aux {
+    int num;
+    struct aux * direita;
+    struct aux * esquerda;
+} Elemento;
+
+typedef struct {
+    Elemento * raiz;
+}ArvoreB;
+
+void Inicializar(ArvoreB * arv);
+int Inserir(ArvoreB * arv, int num);
+void imprimirArvore(Elemento *raiz);
+
 
 #endif // ARVOREB_H_INCLUDED
--- 9_26_07_2023/ArvoreB/main.c
+++ 9_26_07_2023/ArvoreB/main.c
@@ -1,8 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
 int main()
 {
-    printf("Hello world!\n");
+    ArvoreB arvore;
+    Inicializar(&arvore);
+
+    Inserir(&arvore, 5);
+    Inserir(&arvore, 7);
+    Inserir(&arvore, 3);
+    Inserir(&arvore, 4);
+
+    imprimirArvore(arvore.raiz);
+    int c = Contador(arvore.raiz);
+    printf("\nN de elementos: %d", c);
+
     return 0;
 }
