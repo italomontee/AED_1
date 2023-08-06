e1bc7fc981664be3b678694f37a7d3b61da36b31
italomontee <igm.eng22@uea.edu.br>
2023-08-06 18:51:31 -0400

    Update main.c

--- 10_28_07_2023/main.c
+++ 10_28_07_2023/main.c
@@ -6,11 +6,25 @@ int main() {
     ArvoreB arvore;
     Inicializar(&arvore);
 
+    Inserirr(&(arvore.raiz), '(');
     Inserirr(&(arvore.raiz), 'a');
+    Inserirr(&(arvore.raiz), '*');
+    Inserirr(&(arvore.raiz), '(');
+    Inserirr(&(arvore.raiz), '(');
     Inserirr(&(arvore.raiz), 'b');
+    Inserirr(&(arvore.raiz), '-');
     Inserirr(&(arvore.raiz), 'c');
+    Inserirr(&(arvore.raiz), ')');
+    Inserirr(&(arvore.raiz), '/');
+    Inserirr(&(arvore.raiz), 'd');
+    Inserirr(&(arvore.raiz), ')');
+    Inserirr(&(arvore.raiz), ')');
 
     imprimirArvoreIn(arvore.raiz);
+    printf("\n");
+    imprimirArvorePre(arvore.raiz);
+    printf("\n");
+    imprimirArvorePos(arvore.raiz);
     printf("\n");
 
     return 0;
